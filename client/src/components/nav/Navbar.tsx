a74546fcc4995ce1b5bc764a22853d6514eb2b63
Filip Papiernik <filippapiernik1999@gmail.com>
2022-10-27 18:23:00 +0200

    Added authentication part of navbar
    Added avatar component which is rendered when user is logged

--- client/src/components/nav/Navbar.tsx
+++ client/src/components/nav/Navbar.tsx
@@ -1,18 +1,55 @@
+import { LogoDev, Menu as MenuIcon } from '@mui/icons-material';
 import {
   AppBar,
   Toolbar,
   Typography,
   Button,
+  IconButton,
   MenuItem,
+  Menu,
   Box,
+  Tooltip,
+  Avatar,
 } from '@mui/material';
+import { useState } from 'react';
 
 const Navbar = () => {
   const pages = ['Explore', 'Advertise', 'Imaggy+'];
+  const menuItems = [
+    'Profile',
+    'My posts',
+    'My likes',
+    'Go Imaggy+',
+    'Account Settings',
+    'Sign out',
+  ];
+  const isLogged = true;
+  const userFullName = 'Filip Papiernik';
+  const userAvatar = '/images/avatar.jpg';
+  const [menu, setMenu] = useState<null | HTMLElement>(null);
+
+  const handleOpenMenu = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    setMenu(e.currentTarget);
+  };
+
+  const handleCloseMenu = () => {
+    setMenu(null);
+  };
 
   return (
     <AppBar position="static" sx={{ backgroundColor: '#24242424' }}>
       <Toolbar>
+        <IconButton
+          sx={{ display: { xs: 'flex', md: 'none' } }}
+          size="large"
+          color="primary"
+          aria-label="open menu"
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6">Imaggy</Typography>
         <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
           {pages.map((page) => (
             <MenuItem key={page}>
@@ -20,9 +57,45 @@ const Navbar = () => {
             </MenuItem>
           ))}
         </Box>
-        <Box sx={{ display: 'flex', gap: '1em' }}>
-          <Button variant="text">Login</Button>
-          <Button variant="contained">Register</Button>
+        <Box
+          sx={{
+            display: 'flex',
+            gap: '1em',
+            flexGrow: 1,
+            justifyContent: 'flex-end',
+          }}
+        >
+          {isLogged ? (
+            <>
+              <Tooltip title="Open user settings">
+                <IconButton
+                  aria-label="open user settings"
+                  onClick={handleOpenMenu}
+                >
+                  <Avatar alt={userFullName} src={userAvatar}>
+                    {userFullName[0]}
+                  </Avatar>
+                </IconButton>
+              </Tooltip>
+              <Menu
+                anchorEl={menu}
+                keepMounted
+                open={Boolean(menu)}
+                onClose={handleCloseMenu}
+              >
+                {menuItems.map((menuItem) => (
+                  <MenuItem key={menuItem} onClick={handleCloseMenu}>
+                    {menuItem}
+                  </MenuItem>
+                ))}
+              </Menu>
+            </>
+          ) : (
+            <>
+              <Button variant="text">Sign in</Button>
+              <Button variant="contained">Sign up</Button>
+            </>
+          )}
         </Box>
       </Toolbar>
     </AppBar>
