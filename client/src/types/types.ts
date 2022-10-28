aad9d994ad77695559a5ff224732937cf9f8edc3
Filip Papiernik <filippapiernik1999@gmail.com>
2022-10-28 13:51:49 +0200

    Created Comment and Post types, updated User type

--- client/src/types/types.ts
+++ client/src/types/types.ts
@@ -3,16 +3,40 @@ export interface UserInterface {
   email: string;
   name: string;
   surname: string;
-  avatar: string;
+  avatar?: string;
   bio: string;
-  skills: string[] | [];
-  interest: string[] | [];
+  skills: string[];
+  interest: string[];
   links: {
     facebook: string;
     instagram: string;
     website: string;
   };
   location?: string;
-  followers: string[] | [];
-  following: string[] | [];
+  followers: UserInterface[];
+  following: UserInterface[];
+  likedPosts: PostInterface[];
+}
+
+export type CommentType = {
+  _id: number;
+  text: string;
+  userId: string;
+  postId: string;
+  likes: UserInterface[];
+};
+
+export interface PostInterface {
+  _id: number;
+  title: string;
+  image: string;
+  location: string;
+  tags: string[];
+  description: string;
+  views: number;
+  downloads: number;
+  device: string;
+  users: UserInterface[];
+  likes: UserInterface[];
+  comments: CommentType[];
 }
