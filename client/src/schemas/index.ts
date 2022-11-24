aa6b794b7093a177d2584a3a33d2a3d490ba10ea
Filip Papiernik <filippapiernik1999@gmail.com>
2022-11-24 20:30:08 +0100

    Added closeAccountSchema (was missing from previous commit)

--- client/src/schemas/index.ts
+++ client/src/schemas/index.ts
@@ -50,3 +50,11 @@ export const changePasswordSchema = yup.object().shape({
     .oneOf([yup.ref('newPassword'), null], 'Password must match')
     .required('Repeat password is required'),
 });
+
+export const closeAccountSchema = yup.object().shape({
+  currentPassword: yup
+    .string()
+    .min(8, 'Current password must be at least 8 characters')
+    .max(24, 'Current password must be at most 24 characters')
+    .required('Current password is required'),
+});
